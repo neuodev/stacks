d2a1125d5bd535a7d3ef466cfe30e1e42f3d5d2d
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-31 10:26:48 +0200

    Add push method to the stack class

--- stack.js
+++ stack.js
@@ -10,4 +10,21 @@ class Stack {
     this.top = null;
     this.length = 0;
   }
+  push(val) {
+    const node = new Node(val);
+    if (this.top === null) {
+      this.top = node;
+    } else {
+      node.next = this.top;
+      this.top = node;
+    }
+    this.length++;
+    return node;
+  }
 }
+
+const stack = new Stack();
+console.log(stack.push(1));
+console.log(stack.push(2));
+console.log(stack.push(3));
+console.log(stack);
