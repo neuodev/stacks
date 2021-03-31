6c342b77112cb7cafbb6bb5b80c55a587a411058
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-31 08:54:21 +0200

    Add comments

--- Reversing_a_string.ts
+++ Reversing_a_string.ts
@@ -2,14 +2,17 @@ function reverseString(str: any) {
   const stack = [];
   let start = 0;
   let end = str.length - 1;
+  // convert to an array
   str = str.split('');
   while (end > start) {
+    
     stack.push(str[start]);
+    // get the end elment a head 
     str[start] = str[end];
     start++;
     end--;
   }
-
+ // compelte reversing 
   while (stack.length !== 0) {
     str[start] = stack.pop();
     start++;
