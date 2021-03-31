4f7dd369249699f1c64f2adfc2ba14542eb5344d
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-31 08:58:04 +0200

    Cover the edge cases

--- Reversing_a_string.ts
+++ Reversing_a_string.ts
@@ -1,26 +1,27 @@
 function reverseString(str: any) {
+  if (str === null) return false;
   const stack = [];
   let start = 0;
   let end = str.length - 1;
   // convert to an array
   str = str.split('');
   while (end > start) {
-    
     stack.push(str[start]);
-    // get the end elment a head 
+    // get the end elment a head
     str[start] = str[end];
     start++;
     end--;
   }
- // compelte reversing 
+  // compelte reversing
   while (stack.length !== 0) {
     str[start] = stack.pop();
     start++;
   }
 
   str = str.join('');
+  console.log(str);
 
-  console.log(str, stack);
+  return str;
 }
 
-reverseString('abcd');
+reverseString('null');
